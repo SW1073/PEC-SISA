c61b2eaf91b1988f8d6edd87b3aed0da01888ba5
El0b1 <El0b1>
2024-02-27 12:59:15 +0100

    Banco de registros hecho

--- e1_proc_base/src/banco_regs/regfile.vhd
+++ e1_proc_base/src/banco_regs/regfile.vhd
@@ -15,10 +15,22 @@ END regfile;
 
 ARCHITECTURE Structure OF regfile IS
     -- Aqui iria la definicion de los registros
+	 type Mat is array (7 downto 0) of std_logic_vector(15 downto 0);
+	 signal registers : Mat := (others => (others => '0'));
 BEGIN
 
     -- Aqui iria la definicion del comportamiento del banco de registros
     -- Os puede ser util usar la funcion "conv_integer" o "to_integer"
-    -- Una buena (y limpia) implementacion no deberia ocupar más de 7 o 8 lineas
+    -- Una buena (y limpia) implementacion no deberia ocupar mï¿½s de 7 o 8 lineas
+	 
+	 process (clk) is
+	 begin
+		if rising_edge(clk) then
+			if wrd = '1' then
+				registers(to_integer(signed(addr_d))) <= d;
+			end if;
+			a <= registers(to_integer(signed(addr_a)));
+		end if;
+	 end process;
 
 END Structure;
