92a7b5ccd59f3b7a427704c3c467ad6311afa9b6
SW1073 <eloiimerino@gmail.com>
2024-03-16 17:43:57 +0100

    Creo que writes funcionan, pero no sé si la memoria da lo que debería dar

--- e2_proc_mem_sub/Etapa 2.2/src/mem_ctrl/SRAMController.vhd
+++ e2_proc_mem_sub/Etapa 2.2/src/mem_ctrl/SRAMController.vhd
@@ -23,27 +23,10 @@ end SRAMController;
 
 architecture comportament of SRAMController is
 
-    type State_t is (IDLE); -- TODO: Añadir más estados
-    signal s_state: State_t := IDLE;
+    type State_t is (READ, WRITE_SETUP, WRITE);
+    signal s_state: State_t := READ;
+
 begin
--- Así pues, las entradas de la SRAMController son:
---   - clk: La señal de reloj.
---   - address: Es la dirección de memoria a la que el procesador SISA desea acceder (de 16 bits).
---   - dataToWrite: Es el valor procedente del procesador que se escribirá en la memoria en el caso de hacer un store.
---   - WR: Señal que le indica a la memoria si debe leer o escribir un valor.
---   - byte_m: Señal que le indica a la memoria si ha de realizar un acceso a un word o debe realizar un acceso a un byte y extenderlo de signo.
-
--- Las salidas de la SRAMController son:
---   - SRAM_UB_N, SRAM_LB_N, SRAM_CE_N, SRAM_OE_N y SRAM_WE_N: Son las señales de control de la memoria descritas en el datasheet del fabricante.
---   - SRAM_ADDR: Es el bus de direcciones correspondiente al chip de memoria. Su valor depende de donde hayamos mapeado los 64KB de memoria del
---     procesador SISA. Su ancho depende del modelo de placa de desarrollo que estemos usando.
---   - dataReaded: Es el valor que se ha leído de la memoria. En caso de que se hubiese leído un byte se habría extendido el signo.
-
--- El bus SRAM_DQ es un bus bidireccional (de entrada y salida) de la SRAMController por el que van los datos hacia y
--- desde el chip de memoria. Al ser un bus bidireccional implica que diversos componentes pondrán datos en él en distintos
--- momentos. Hemos de garantizar en el diseño que dos componentes nunca pongan un valor en el bus de forma simultánea.
--- Por ello, cuando un componente no desee escribir en el bus deberá poner sus salidas en alta impedancia (High-Z)
-
 
 	SRAM_ADDR <= "00" & address;
 	SRAM_CE_N <= '0';
@@ -58,24 +41,55 @@ begin
         if rising_edge(clk) then
             v_state := s_state;
             case (s_state) is
-                when IDLE =>
-                    v_state := IDLE;
+                when READ =>
+					if WR = '1' then
+						v_state := WRITE_SETUP;
+					end if;
+
+				when WRITE_SETUP =>
+					v_state := WRITE;
+
+				when WRITE =>
+					if WR = '0' then
+						v_state := READ;
+					end if;
+
             end case;
             s_state <= v_state;
         end if;
     end process; -- next_state
 
+
     output_logic: process(clk) is
     begin
         if rising_edge(clk) then
             case (s_state) is
-                when IDLE =>
-                    SRAM_UB_N <= '0';
-                    SRAM_LB_N <= '0';
-                    SRAM_WE_N <= '1';
-					SRAM_DQ <= (others => 'Z');
-                    -- if next state is gona be READ, hacer las cosaas de read
-                    -- COMO EN MP XD
+                when READ =>
+					if WR = '1' then
+						SRAM_UB_N <= byte_m;
+						SRAM_LB_N <= '0';
+						SRAM_WE_N <= '0';
+						SRAM_DQ <= dataToWrite;
+					else
+						SRAM_UB_N <= '0';
+						SRAM_LB_N <= '0';
+						SRAM_WE_N <= '1';
+						SRAM_DQ <= (others => 'Z');
+					end if;
+
+				when WRITE_SETUP =>
+					SRAM_WE_N <= '1';
+					SRAM_LB_N <= '1';
+					SRAM_UB_N <= '1';
+
+				when WRITE =>
+					if WR= '0' then
+						SRAM_UB_N <= '0';
+						SRAM_LB_N <= '0';
+						SRAM_WE_N <= '1';
+						SRAM_DQ <= (others => 'Z');
+					end if;
+
             end case;
         end if;
     end process; -- output_logic
