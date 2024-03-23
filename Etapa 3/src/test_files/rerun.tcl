3e1fa2a2759df9728c0fc8bb94a9cbddd83e703b
SW1073 <eloiimerino@gmail.com>
2024-03-23 20:44:41 +0100

    la vida es una lenteja. nuevos scripts para recompilar y volver a correr and everything

---
+++ Etapa 3/src/test_files/rerun.tcl
@@ -0,0 +1,3 @@
+do compile.tcl
+restart -f
+run 12100ns
--- Etapa 3/src/test_files/runsim.tcl
+++ Etapa 3/src/test_files/runsim.tcl
@@ -7,27 +7,7 @@ if {[file exists rtl_work]} {
 vlib rtl_work
 vmap work rtl_work
 
-vcom -93 -work work {./package_timing.vhd}
-vcom -93 -work work {./package_utility.vhd}
-vcom -93 -work work {./async_64Kx16.vhd}
-vcom -93 -work work {./test_sisa.vhd}
-
-vcom -93 -work work {../proc/alu/op_arit_log.vhd}
-vcom -93 -work work {../proc/alu/op_comp.vhd}
-vcom -93 -work work {../proc/alu/op_ext_arit.vhd}
-vcom -93 -work work {../proc/alu/op_immed.vhd}
-vcom -93 -work work {../proc/alu/op_movs.vhd}
-vcom -93 -work work {../driver7Segmentos.vhd}
-vcom -93 -work work {../sisa.vhd}
-vcom -93 -work work {../mem_ctrl/SRAMController.vhd}
-vcom -93 -work work {../mem_ctrl/MemoryController.vhd}
-vcom -93 -work work {../proc/alu/alu.vhd}
-vcom -93 -work work {../proc/banco_regs/regfile.vhd}
-vcom -93 -work work {../proc/datapath/datapath.vhd}
-vcom -93 -work work {../proc/uc/unidad_control.vhd}
-vcom -93 -work work {../proc/uc/multi.vhd}
-vcom -93 -work work {../proc/uc/control_l.vhd}
-vcom -93 -work work {../proc/proc.vhd}
+do compile.tcl
 
 vsim work.test_sisa
 
