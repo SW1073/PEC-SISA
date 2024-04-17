c20ef053a3745ad3c80c477f5ff97214d8f6585c
SW1073 <eloiimerino@gmail.com>
2024-04-17 11:44:32 +0200

    compile update

--- Etapa 6/src/test_files/compile.tcl
+++ Etapa 6/src/test_files/compile.tcl
@@ -3,6 +3,8 @@ vcom -93 -work work {./package_utility.vhd}
 vcom -93 -work work {../proc/packages/package_opcodes.vhd}
 vcom -93 -work work {../proc/packages/package_alu.vhd}
 vcom -93 -work work {../proc/packages/package_control.vhd}
+vcom -93 -work work {../package_records.vhd}
+vcom -93 -work work {../package_records.vhd}
 vcom -93 -work work {./async_64Kx16.vhd}
 vcom -93 -work work {./test_sisa.vhd}
 
