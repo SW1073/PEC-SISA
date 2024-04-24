7adfcbf73994645350e1098c762d36293127366f
El0b1 <El0b1>
2024-04-24 13:54:19 +0200

    funciona el snake. jl salvando vidas

--- Etapa 6/src/io_ctrl/controladores_io.vhd
+++ Etapa 6/src/io_ctrl/controladores_io.vhd
@@ -42,31 +42,27 @@ ARCHITECTURE Structure OF controladores_IO IS
     SIGNAL s_ps2_clear_char : std_logic;
     SIGNAL s_ps2_data_ready : std_logic;
 
-    SIGNAL contador_ciclos          : std_logic_vector(15 downto 0);
-    SIGNAL contador_milisegundos    : std_logic_vector(15 downto 0);
 BEGIN
 
-    process(CLOCK_50)
-    begin
-        if rising_edge(CLOCK_50) then
-            if contador_ciclos=0 then
-                contador_ciclos<=x"C350"; -- tiempo de ciclo=20ns(50Mhz) 1ms=50000ciclos
-                if contador_milisegundos>0 then
-                    contador_milisegundos <= contador_milisegundos-1;
-                end if;
-            else
-                contador_ciclos <= contador_ciclos-1;
-            end if;
-        end if;
-    end process;
-
     PROCESS (CLOCK_50) IS
+        variable v_contador_milisegundos : std_logic_vector(15 downto 0) := x"0000";
+        variable v_contador_ciclos       : std_logic_vector(15 downto 0) := x"0000";
     BEGIN
         IF rising_edge(CLOCK_50) THEN
             registers(PORT_KEY)(3 downto 0) <= KEY(3 DOWNTO 0);
             registers(PORT_SW)(7 downto 0) <= SW(7 DOWNTO 0);
-            registers(PORT_RAND) <= contador_ciclos;
-            registers(PORT_TIMER) <= contador_milisegundos;
+            
+            if v_contador_ciclos = 0 then
+                v_contador_ciclos := x"C350"; -- tiempo de ciclo=20ns(50Mhz) 1ms=50000ciclos
+                if v_contador_milisegundos > 0 then
+                    v_contador_milisegundos := v_contador_milisegundos - 1;
+                end if;
+            else
+                v_contador_ciclos := v_contador_ciclos - 1;
+            end if;
+
+            registers(PORT_TIMER) <= v_contador_milisegundos;
+            registers(PORT_RAND) <= v_contador_ciclos;
 
             IF wr_out = '1' AND addr_io /= PORT_KEY AND addr_io /= PORT_SW THEN
                 if addr_io = PORT_PS2_DATA_VALID THEN
@@ -74,6 +70,9 @@ BEGIN
                     s_ps2_clear_char <= '0';
                 ELSE
                     registers(conv_integer(addr_io)) <= wr_io;
+                    if addr_io = PORT_TIMER then
+                        v_contador_milisegundos := wr_io;
+                    end if;
                 END IF;
             END IF;
 
