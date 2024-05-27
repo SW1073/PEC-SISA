2aeda2ddeb1b150581ac025b00e0da2b33b828d1
SW1073 <eloiimerino@gmail.com>
2024-05-27 08:46:04 +0200

    Fix (un poco guarro) de exception controller

--- Etapa 8/src/proc/uc/exception_ctrl.vhd
+++ Etapa 8/src/proc/uc/exception_ctrl.vhd
@@ -38,7 +38,15 @@ ARCHITECTURE Structure OF exception_ctrl IS
     SIGNAL s_tlb_invalid   : std_logic;
     SIGNAL s_tlb_readonly  : std_logic;
 
+    SIGNAL s_is_fetch           : std_logic;
+    SIGNAL s_is_demw            : std_logic;
+    SIGNAL s_is_demw_mem_access : std_logic;
+
 BEGIN
+
+    s_is_fetch           <= not ins_dad;
+    s_is_demw            <= ins_dad;
+    s_is_demw_mem_access <= is_mem_access AND s_is_demw;
 
     s_bad_alignment <= is_mem_access AND
                        not word_byte AND
@@ -75,7 +83,8 @@ BEGIN
                       is_mem_access     AND
                       wr_m;
 
-    exception.is_exception <= '1' WHEN (ins_dad = '1' AND (     -- DEMW
+    exception.is_exception <= '1' WHEN (s_is_demw = '1' AND (
+                                       -- DEMW
                                            div_by_zero = '1'       OR
                                            is_illegal_ir = '1'     OR
                                            s_bad_alignment = '1'   OR
@@ -83,32 +92,36 @@ BEGIN
                                            s_protected_ir = '1'    OR
                                            s_calls = '1'
                                        ))
-                                       OR (                        -- Both FETCH or DEMW
-                                           s_tlb_miss = '1'        OR
-                                           s_protected_mem = '1'   OR
-                                           s_tlb_invalid = '1'     OR
+                                       -- Both FETCH or DEMW
+                                       OR (
                                            s_interrupt = '1'
+                                       )
+                                       -- Fetch, or DEMW with memory access
+                                       OR ((s_is_demw_mem_access = '1' OR s_is_fetch = '1') AND
+                                           (s_tlb_miss = '1'        OR
+                                            s_protected_mem = '1'   OR
+                                            s_tlb_invalid = '1')
                                        )
                               ELSE '0';
 
-    exception.code <=   EX_DIV_BY_ZERO      WHEN div_by_zero = '1'      AND ins_dad = '1'   ELSE
-                        EX_ILLEGAL_INSTR    WHEN is_illegal_ir = '1'    AND ins_dad = '1'   ELSE
-                        EX_BAD_ALIGNMENT    WHEN s_bad_alignment = '1'  AND ins_dad = '1'   ELSE
+    exception.code <=   EX_DIV_BY_ZERO      WHEN div_by_zero = '1'      AND s_is_demw = '1'             ELSE
+                        EX_ILLEGAL_INSTR    WHEN is_illegal_ir = '1'    AND s_is_demw = '1'             ELSE
+                        EX_BAD_ALIGNMENT    WHEN s_bad_alignment = '1'  AND s_is_demw = '1'             ELSE
                         -- TLB
-                        EX_MISS_ITLB        WHEN s_tlb_miss = '1'       AND ins_dad = '0'   ELSE
-                        EX_MISS_DTLB        WHEN s_tlb_miss = '1'       AND ins_dad = '1'   ELSE
+                        EX_MISS_ITLB        WHEN s_tlb_miss = '1'       AND s_is_fetch = '1'            ELSE
+                        EX_MISS_DTLB        WHEN s_tlb_miss = '1'       AND s_is_demw_mem_access = '1'  ELSE
 
-                        EX_INVALID_ITLB     WHEN s_tlb_invalid = '1'    AND ins_dad = '0'   ELSE
-                        EX_INVALID_DTLB     WHEN s_tlb_invalid = '1'    AND ins_dad = '1'   ELSE
+                        EX_INVALID_ITLB     WHEN s_tlb_invalid = '1'    AND s_is_fetch = '1'            ELSE
+                        EX_INVALID_DTLB     WHEN s_tlb_invalid = '1'    AND s_is_demw_mem_access = '1'  ELSE
 
-                        EX_PROTECTED_ITLB   WHEN s_protected_mem = '1'  AND ins_dad = '0'   ELSE
-                        EX_PROTECTED_DTLB   WHEN s_protected_mem = '1'  AND ins_dad = '1'   ELSE
+                        EX_PROTECTED_ITLB   WHEN s_protected_mem = '1'  AND s_is_fetch = '1'            ELSE
+                        EX_PROTECTED_DTLB   WHEN s_protected_mem = '1'  AND s_is_demw_mem_access = '1'  ELSE
 
-                        EX_READONLY_DTLB    WHEN s_tlb_readonly = '1'   AND ins_dad = '1'   ELSE
+                        EX_READONLY_DTLB    WHEN s_tlb_readonly = '1'   AND s_is_demw_mem_access = '1'  ELSE
                         -- NON EXCEPTION EXCEPTIONS
-                        EX_PROTECTED_IR     WHEN s_protected_ir = '1'   AND ins_dad = '1'   ELSE
-                        EX_CALLS            WHEN s_calls = '1'          AND ins_dad = '1'   ELSE
-                        EX_INTERRUPT_CODE   WHEN s_interrupt = '1'                          ELSE
+                        EX_PROTECTED_IR     WHEN s_protected_ir = '1'   AND s_is_demw = '1'             ELSE
+                        EX_CALLS            WHEN s_calls = '1'          AND s_is_demw = '1'             ELSE
+                        EX_INTERRUPT_CODE   WHEN s_interrupt = '1'                                      ELSE
                         (OTHERS => 'X');
 
 END Structure;
