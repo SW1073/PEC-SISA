28ab473664b6ef37286b44d23f143319531dacfc
SW1073 <eloiimerino@gmail.com>
2024-05-29 23:09:23 +0200

    todos los tests menos exceptions 10

---
+++ tests/exception_test/1/alineacion_impar.s
@@ -0,0 +1,67 @@
+.macro $movei p1 imm16
+        movi    \p1, lo(\imm16)
+        movhi   \p1, hi(\imm16)
+.endm
+
+
+.text
+       ; *=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*
+       ; Inicializacion
+       ; *=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*
+       di                 ;desactiva las interrupciones (o se asegura que lo esten)
+       $MOVEI r1, RSG
+       wrs    s5, r1      ;inicializamos en S5 la direccion de la rutina de antencion a las excepciones
+       movi   r1, 0xF
+       out     9, r1      ;activa todos los visores hexadecimales
+       movi   r1, 0xFF
+       out    10, r1      ;muestra el valor 0xFFFF en los visores
+       $MOVEI r6, inici   ;adreça de la rutina principal
+       jmp    r6
+
+       ; *=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*
+       ; Rutina de servicio de interrupcion
+       ; *=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*
+RSG:   rds    r7, s2      ;obtiene el valor del registro de estado. Nos dice que excepción ha ocurrido
+       addi   r5, r5, 1
+       out    10, r5      ;muestra el numero de excepciones atendidas hasta en el momento
+       rds    r7, s3
+       ; Si el la excepción provocada por FETCH, halt
+       ; else reti
+       $MOVEI r4, fin
+       movi   r6, -12
+       shl    r7, r7, r6
+       add    r7, r7, r6
+       jz     r7, r4
+       reti
+fin:
+       halt
+
+
+       ; *=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*
+       ; Rutina principal
+       ; *=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*
+inici:
+       movi   r0, 0xAA
+       movhi  r0, 0xAA
+
+       ; ST y LD (legales)
+       movi   r1, 0
+       st    0(r1), r0
+       st    2(r1), r0
+
+       ; ST y LD (ilegales)
+       movi   r1, 1
+       st     0(r1), r2
+       ld     r2, 0(r1)
+
+       ; STB y LDB (legales)
+       movi   r1, 1
+       stb    0(r1), r0
+       ldb    r2, 0(r1)
+
+       ; excepción de pc mal alineado en FETCH (ilegal)
+       movi   r1, 0x01
+       movhi  r1, 0xC0
+       jmp    r1
+       ; no deberíamos llegar a este halt si se ha hecho bien
+       halt
